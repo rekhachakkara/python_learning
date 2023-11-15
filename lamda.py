388c7547f5c3727acfc3b5a2e6805633ba512813
Rekha Chakkaravarthy <rekhachakkaravarthy@gmail.com>
2023-11-14 18:53:11 -0600

    lambda  commit

---
+++ lamda.py
@@ -0,0 +1,7 @@
+'lamda is a anaonyonus function which can have many arguments but can have only one expression'
+
+x = lambda a : a + 10
+print(x (5))
+
+y = lambda b :  b + abs(2.5)
+print(y(2.33))
